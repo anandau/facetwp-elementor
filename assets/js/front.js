ce6fef074eeab97849cb6427f9026d4a084470ef
Jenny Ragan <ruwebtech@gmail.com>
2020-01-21 12:37:04 -0500

    update js for posts widget navigation

--- assets/js/front.js
+++ assets/js/front.js
@@ -14,11 +14,9 @@
     $(document).on('click', '.facetwp-template.elementor-widget-posts .elementor-pagination a', function(e) {
         e.preventDefault();
         var matches = $(this).attr('href').match(/\/(\d+)/);
-        if (null !== matches) {
-            FWP.paged = parseInt(matches[1]);
-            FWP.soft_refresh = true;
-            FWP.refresh();
-        }
+        FWP.paged = null !== matches ? parseInt(matches[1]) : 1;
+        FWP.soft_refresh = true;
+        FWP.refresh();
     });
     $(document).on('click', '.facetwp-template .woocommerce-pagination a', function(e) {
         e.preventDefault();
